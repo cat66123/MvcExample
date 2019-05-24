531ada458e585aeb9efa0aa74bde8dc4a16b7c93
cat66123 <326700510@qq.com>
2019-05-24 15:38:38 +0800

    v0.0.1

---
+++ WebMvc/WebMvc/Controllers/UserLoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        public IActionResult Index(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                //数据验证
+                return Redirect("/UserLogin/LoginData");
+            }
+            return View();
+        }
+
+        public IActionResult LoginData()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebMvc/WebMvc/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "用户名不能为空")]
+        [DisplayName("用户名")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="密码不能为空")]
+        [DisplayName("密码")]
+        public string Pwd { get; set; }
+    }
+}
